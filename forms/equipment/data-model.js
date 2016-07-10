a7014716a5771d799eac9a30113d4e484c1613d0
Alexander Thohoh <jerk401@gmail.com>
2016-07-10 16:00:41 +0300

    init

---
+++ forms/equipment/data-model.js
@@ -0,0 +1,45 @@
+{
+    "technicalParameters": [
+        {
+            "emission": 10,
+            "bandwidth": 20,
+            "rxData": [
+                {"frequencyOffset": 1, "value": 1},
+                {"frequencyOffset": 5, "value": 2},
+                {"frequencyOffset": 7, "value": 3},
+                {"frequencyOffset": 8, "value": 4}
+            ],
+            "txData": [
+                {"frequencyOffset": 0, "value": 8},
+                {"frequencyOffset": 3, "value": 6.5},
+                {"frequencyOffset": 6, "value": 3},
+                {"frequencyOffset": 9, "value": 4}
+            ]
+        },
+        {
+            "emission": 20,
+            "bandwidth": 40,
+            "rxData": [
+                {"frequencyOffset": 4, "value": 1},
+                {"frequencyOffset": 5, "value": 2},
+                {"frequencyOffset": 7, "value": 3},
+                {"frequencyOffset": 8, "value": 4}
+            ],
+            "txData": [
+                {"frequencyOffset": 0, "value": 8},
+                {"frequencyOffset": 3, "value": 6.5},
+                {"frequencyOffset": 9, "value": 9},
+                {"frequencyOffset": 10, "value": 4}
+            ]
+        }
+    ],
+    "polarization": [],
+    "electricalDowntilt": [],
+    "pattern": [
+        {
+            "frequency": 1,
+            "gain": 2,
+            "eTilt": 3
+        }
+    ]
+}
